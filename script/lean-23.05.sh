09fb436a40918f83fbd354f6982a042c9f5f9d9b
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-01-27 10:07:27 +0800

    Update lean-23.05.sh

--- script/lean-23.05.sh
+++ script/lean-23.05.sh
@@ -27,6 +27,9 @@ git clone --depth 1 https://github.com/destan19/OpenAppFilter.git  package/oaf
 git clone --depth 1 https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 git clone --depth 1 https://github.com/lwb1978/openwrt-gecoosac.git package/openwrt-gecoosac
 
+git clone --depth 1 https://github.com/sbwml/luci-app-mentohust package/mentohust
+
+
 rm -rf feeds/packages/lang/golang
 git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 rm -rf feeds/luci/applications/{luci-app-openclash,luci-app-passwall,luci-app-passwall2,luci-app-mosdns}
@@ -58,14 +61,14 @@ mv package/kz8-small/luci-app-tailscale package/luci-app-tailscale
 mv package/kz8-small/tailscale package/tailscale
 rm -rf package/kz8-small
 
-rm -rf feeds/packages/net/mentohust
-git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/packages package/imm23pkg
-mv package/imm23pkg/net/mentohust package/mentohust
-rm -rf package/imm23pkg
+#rm -rf feeds/packages/net/mentohust
+#git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/packages package/imm23pkg
+#mv package/imm23pkg/net/mentohust package/mentohust
+#rm -rf package/imm23pkg
 
-rm -rf feeds/luci/applications/luci-app-mentohust
-git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci package/imm23luci
-mv package/imm23luci/applications/luci-app-mentohust package/luci-app-mentohust
-rm -rf package/imm23luci
+#rm -rf feeds/luci/applications/luci-app-mentohust
+#git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci package/imm23luci
+#mv package/imm23luci/applications/luci-app-mentohust package/luci-app-mentohust
+#rm -rf package/imm23luci
 
 
