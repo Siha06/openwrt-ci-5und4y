06d48b83dff40954c63694e5a825be0e9af39a13
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-03-06 16:35:09 +0800

    Update 199-x86-1.sh

--- patch/immortalwrt-24.10/199-x86-1.sh
+++ patch/immortalwrt-24.10/199-x86-1.sh
@@ -17,8 +17,8 @@ sed -i '/passwall/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
 sed -i '/targets/d' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.4/targets/x86/64/kmods/6.6.110-1-f8c5d7fde74fa4fedf4370775255c515' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz immortalwrt_core https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.4/targets/x86/64/packages' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10.5/targets/x86/64/kmods/6.6.122-1-e7e50fbc0aafa7443418a79928da2602' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz immortalwrt_core https://mirror.nju.edu.cn/immortalwrt/releases/24.10.5/targets/x86/64/packages' /etc/opkg/distfeeds.conf
 
 
 # 计算网卡数量
